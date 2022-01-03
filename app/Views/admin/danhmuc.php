c8c886783e36766eca1eb0694e13e38343cc765d
Hoang Hoa <kieuhoa256@gmail.com>
2022-01-03 21:05:57 +0700

    Edit Link to Home page

--- app/Views/admin/danhmuc.php
+++ app/Views/admin/danhmuc.php
@@ -10,7 +10,7 @@
             <small>Quản lý danh mục</small>
         </h1>
         <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li><a href="/home/index"><i class="fa fa-dashboard"></i> Home</a></li>
             <li class="active">Danh mục</li>
         </ol>
     </section>
